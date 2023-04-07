be23e8f0565aacd7a899340ed2ead13bf82232c3
Mirass120 <mirekretkiewicz@gmail.com>
2023-04-07 15:54:30 +0200

    create Implementacje

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 console.log("home page");
+
+let x = '';
+
+function validateForm() {
+    let input = document.getElementById("myText").value;
+    if (input === "" || input === "Nazwa gracza") {
+      alert("Name must be filled out");
+      return false; //zwraca false aby uniemozliwic wysłanie formularza
+    } else {
+        x = input;
+        location.href ="Loading.html";
+        localStorage.setItem("Nazwa gracza",x);
+        return x;
+    };    
+  };
+
+
+  
+
